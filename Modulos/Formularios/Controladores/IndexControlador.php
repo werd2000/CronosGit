6849f215572ec9656454d7606d4df5afcbc070f8
werd2000 <walter.enrique.ruiz.diaz@gmail.com>
2015-07-16 13:45:14 -0300

    Formularios

--- Modulos/Formularios/Controladores/IndexControlador.php
+++ Modulos/Formularios/Controladores/IndexControlador.php
@@ -1,8 +1,10 @@
 <?php
+require_once BASE_PATH . 'LibQ' . DS . 'BarraHerramientas.php';
+
 /**
  * Clase Formularios Controlador 
  */
-class indexControlador extends formulariosControlador
+class Formularios_Controladores_indexControlador extends Controladores_formulariosControlador
 {
 
     protected $_listaFormularios = array(
@@ -11,43 +13,67 @@ class indexControlador extends formulariosControlador
         'Historia_Clinica_Unica_Para_Alumnos_de_Escuela_Saludable.pdf',
         'Entrevista_Inicial.pdf'
         );
+    private $_paramBotonNuevo = array(
+        'href' => '?option=Formularios&sub=index&cont=nuevo',
+        'classIcono' => 'icono-nuevo32',
+        'titulo' => 'Nuevo',
+        'class' => 'btn btn-primary'
+    );
+
+    /**
+     * Propiedad usada para configurar el boton ELIMINAR
+     * @var type Array
+     */
+    private $_paramBotonEliminar = array(
+        'href' => "\"javascript:void(0);\"",
+        'evento' => "onclick=\"javascript: submitbutton('Eliminar')\"",
+        'class' => 'btn btn-primary'
+    );
+
+    /**
+     * Propiedad usada para configurar el botón VOLVER
+     * @var type Array
+     */
+    private $_paramBotonVolver = array(
+        'href' => "javascript:history.back(1)",
+        'classIcono' => 'icono-volver32',
+        'titulo' => 'Volver',
+        'class' => 'btn btn-primary'
+    );
+
+    /**
+     * Propiedad usa para configurar el botón GUARDAR ALUMNO
+     * @var type Array
+     */
+    private $_paramBotonGuardar = array(
+        'href' => "\"javascript:void(0);\"",
+        'evento' => "onclick=\"javascript: submitbutton('Guardar')\"",
+        'class' => 'btn btn-primary'
+    );
+    private $_paramBotonInicio = array(
+        'href' => "?option=Index",
+        'classIcono' => 'icono-inicio32',
+        'titulo' => 'Inicio',
+        'class' => 'btn btn-primary'
+    );
+
 
     public function __construct()
     {
         parent::__construct();
-//        $this->_formularios = $this->cargarModelo('index');
     }
 
     public function index()
     {
         
-        $menu = array(
-            array(
-                'onclick'=> '',
-                'href'   => "?option=exportExcel&sub=formularios",
-                'title' => 'Exportar',
-                'class'  => 'icono-exportar32'
-            ),
-            array(
-                'onclick'=> '',
-                'href'   => "?option=Formularios&sub=index&cont=nuevo",
-                'title' => 'Nuevo',
-                'class'  => 'icono-nuevo32'
-            ),
-            array(
-                'onclick'=> '',
-                'href'   => "javascript:history.back(1)",
-                'title' => 'Volver',
-                'class'  => 'icono-volver32'
-            ),
-            array(
-                'onclick'=> '',
-                'href'   => "?option=Index",
-                'title' => 'Inicio',
-                'class'  => 'icono-inicio32'
-            )
-        );
-        $this->_vista->_barraHerramientas = $menu;
+        $this->isAutenticado();
+        /** Barra de herramientas */
+        $bh = new LibQ_BarraHerramientas();
+//        $bh->addBoton('Nuevo', $this->_paramBotonNuevo);
+        $bh->addBoton('Volver', $this->_paramBotonVolver);
+        $bh->addBoton('Inicio', $this->_paramBotonInicio);
+        $this->_vista->setVistaCss(array('formularios'));
+        $this->_vista->_barraHerramientas = $bh->render();
         
         $this->_vista->datos = $this->_listaFormularios;
         $this->_vista->titulo = 'Formularios';
